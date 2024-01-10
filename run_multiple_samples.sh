2e46ae830ae2767431bc119a5a809423ac3ec445
Gregjlt <gregoire.jouault@curie.fr>
2024-01-10 15:24:28 +0100

    modifying the 10X barcode whitelist

--- run_multiple_samples.sh
+++ run_multiple_samples.sh
@@ -75,7 +75,7 @@ DESIGN_TYPE=LBC
   # FASTQ_DIR=/data/tmp/gjouault/10X/fastq/${DATASET_NAME}/
   
 
- echo "cd ~/GitLab/scCutTag_10X/; ./schip_processing.sh All -i ${FASTQ_DIR} -d ${DATASET_NAME} -c ${OUTPUT_CONFIG} -o ${OUTPUT_DIR} --name ${FINAL_NAME}" | qsub -l "nodes=1:ppn=8,mem=60gb" -N job_${FINAL_NAME}_${ASSEMBLY}
+ echo "cd ~/GitLab/Custom_scCutTag_10X/; ./schip_processing.sh All -i ${FASTQ_DIR} -d ${DATASET_NAME} -c ${OUTPUT_CONFIG} -o ${OUTPUT_DIR} --name ${FINAL_NAME}" | qsub -l "nodes=1:ppn=8,mem=60gb" -N job_${FINAL_NAME}_${ASSEMBLY}
 
 done < "$sample_sheet"
 
